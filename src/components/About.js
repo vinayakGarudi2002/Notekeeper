9646cceb59f891eb767bf3d6c309cb192b758bd2
vinayak <vinayakgarudi45@gmail.com>
2023-01-07 22:14:13 +0530

    almost done

---
+++ src/components/About.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+const About = () => {
+   
+    
+  return (
+    <div className={"text-center text-primary "}>
+<div class="about-section">
+  <h2>About Notekeeper</h2>
+  <ul>
+    <li>Notekeeper is a web-based application designed to help users easily create, manage, and organize their notes.</li>
+    <li>With a user-friendly interface and powerful features, Notekeeper is the perfect tool for anyone looking to stay organized and on top of their tasks.</li>
+    <li>Our goal is to provide users with a simple yet effective way to keep track of their notes, and we are constantly working to improve and expand the capabilities of the application.</li>
+    <li>Whether you're a student, professional, or anyone in between, we believe that Notekeeper can help you streamline your note-taking and stay organized in today's fast-paced world.</li>
+  </ul>
+</div>
+ 
+    </div>
+  )
+}
+
+export default About
